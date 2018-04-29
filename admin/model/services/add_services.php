86294cafae7dcad027d3409ee01ee1956abd5511
purnamanr <purnama.n.rachman@gmail.com>
2018-04-29 22:14:13 +0700

    add services and services_form, edit clean url and remove sidebar wraper

---
+++ admin/model/services/add_services.php
@@ -0,0 +1,69 @@
+<?php 
+//insert konten layanan
+include '../../../config/configuration.php';
+
+$judul = $_POST['judul'];
+$desc = $_POST['deskripsi'];
+$publish = $_POST['publish'];
+// $image = $_POST['file'];
+
+$file = $_FILES['file'];
+$filename = explode(".", $file["name"]); 
+$file_name = $file['name']; // file original name
+$file_name_no_ext = isset($filename[0]) ? $filename[0] : null; // File name without the extension
+$file_extension = $filename[count($filename)-1];
+$fileNewName = md5( $file_name_no_ext[0].microtime() ).'.'.$file_extension ;
+
+$target_dir = "../../uploads/services/";
+// echo 'ext = '.$target_dir.$fileNewName;
+// $target_file = $target_dir . basename($_FILES["file"]["name"]);
+$target_file = $target_dir . $fileNewName;
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["file"]["size"] > 200000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        // echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+        // echo 'nama file setelah di enkripsi = '.$fileNewName;
+        $location = "uploads/services/".$fileNewName; 
+        mysqli_query($conn,"INSERT into services values ('','$judul','$desc','$publish','$location',now(),now())");
+        $conn->close();
+
+        echo "<script>alert('Simpan berhasil!')
+        location.replace('../../services')</script>";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+?>
---
+++ admin/services_form.php
@@ -0,0 +1,115 @@
+<?php 
+session_start();
+if (!empty($_SESSION['login_username']))
+{
+  $user = $_SESSION['login_username'];
+  $status = $_SESSION['login_status'];
+  include 'admin_header.php';
+  $menu = "Content";
+?>
+<!-- Core stylesheets -->
+<link rel="stylesheet" href="css/form.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript">
+ $(document).ready(function() {
+        $("#fileUpload").on('change', function() {
+          //Get count of selected files
+          var countFiles = $(this)[0].files.length;
+          var imgPath = $(this)[0].value;
+          var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+          var image_holder = $("#image-holder");
+          image_holder.empty();
+          if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+            if (typeof(FileReader) != "undefined") {
+              //loop for each file selected for uploaded.
+              for (var i = 0; i < countFiles; i++) 
+              {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                  $("<img />", {
+                    "src": e.target.result,
+                    "class": "img-thumbnail"
+                  }).appendTo(image_holder);
+                }
+                image_holder.show();
+                reader.readAsDataURL($(this)[0].files[i]);
+              }
+            } else {
+              alert("This browser does not support FileReader.");
+            }
+          } else {
+            alert("Hanya support file dengan extension png, jpg, jpg dan gif.");
+          }
+        });
+      });
+</script>
+<!--====================================================
+                    PAGE CONTENT
+======================================================-->
+<div class="page-content d-flex align-items-stretch">
+  <!--***** SIDE NAVBAR *****-->
+  <?php include 'sidebar_menu.php'; ?>
+
+  <div class="content-inner form-cont">
+    <div class="row">
+      <div class="col-md-12">
+      <!--***** BASIC FORM *****-->
+        <div class="card form" id="form1">
+            <div class="card-header">
+                <h3>Services Form</h3>
+            </div>
+            <br>
+            <form method="post" action="model/services/add-services" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-md-6">
+                      <br>
+                        <div class="form-group">
+                            <label for="judulLayanan">Judul Layanan</label>
+                            <input type="text" class="form-control" name="judul" maxlength="75">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleTextarea">Deskripsi Layanan</label>
+                            <textarea class="form-control" id="exampleTextarea" rows="4" name="deskripsi" maxlength="1000"></textarea>
+                            <small id="fileHelp" class="form-text text-muted">Maksimal 1000 karakter.</small>
+                        </div>
+                        <fieldset class="form-group">
+                            <label>Publikasikan</label>
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input type="radio" class="mb-2 mr-sm-1 mb-sm-0" name="publish" id="optionsRadios1" value="ya" checked> Ya &emsp;
+                                    <input type="radio" class="mb-2 mr-sm-1 mb-sm-0" name="publish" id="optionsRadios2" value="tidak"> Tidak
+                                </label>
+                            </div>
+                        </fieldset>        
+                    </div>
+                    <div class="col-md-6">
+                      <br>
+                        <div class="form-group">
+                            <label for="exampleInputFile">Gambar Profile Layanan</label>
+                            <!--input type="file"  class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp"  onchange="readURL(this);" -->
+                            <input id="fileUpload"  class="form-control-file" multiple="multiple" name="file" type="file"/> 
+                            <small id="fileHelp" class="form-text text-muted">Maksimal size 2 Mb.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleTextarea">Preview Gambar</label><br>
+                            <div id="image-holder"></div>
+                        </div>
+                     </div>
+                </div> 
+                <button type="submit" class="btn btn-general btn-blue mr-2">Submit</button>  
+                <button type="reset" class="btn btn-general btn-white">Cancel</button>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php 
+  include 'admin_footer.php';
+} 
+else { ?>
+  <script> alert('Anda tidak mempunyai hak akses! Hubungi Administrator.')
+  document.location="http://<?php echo $_SERVER['HTTP_HOST']?>/berkahsantoso/"
+  </script>
+<?php } ?>
