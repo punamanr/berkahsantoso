4132d585af42db0752b11e976d8c4f11fac5b209
purnamanr <purnama.n.rachman@gmail.com>
2018-05-03 14:51:18 +0700

    add delete article modul

--- config/configuration.php
+++ config/configuration.php
@@ -1,8 +1,13 @@
 <?php 
-$host = "us-cdbr-iron-east-05.cleardb.net";
-$database = "heroku_58aec591222702d";
-$username = "bcbb5fbcd91d77";
-$password = "a1232cea";
+$host = "localhost";
+$database = "db_berkahsantoso";
+$username = "root";
+$password = "";
+
+// $host = "us-cdbr-iron-east-05.cleardb.net";
+// $database = "heroku_58aec591222702d";
+// $username = "bcbb5fbcd91d77";
+// $password = "a1232cea";
 
 // Create connection (OOP)
 $conn = new mysqli($host, $username, $password, $database);// Check connection
