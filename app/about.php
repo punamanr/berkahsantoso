6ea3636cf723c63a603ccb9f1a9fde4076eab33f
purnamanr <purnama.n.rachman@gmail.com>
2018-05-04 07:24:29 +0700

    add about page

--- app/about.php
+++ app/about.php
@@ -6,10 +6,9 @@ include 'header.php';
 <!--====================================================
                        HOME-P
 ======================================================-->
-    <div id="home" class="home-p pages-head1 text-center">
+    <div id="home" class="home-p pages-head5 text-center">
         <div class="container">
-            <h1 class="wow fadeInUp" data-wow-delay="0.1s">Businessbox is fully responsive and Clean</h1>
-            <p>Discover more about us</p>
+
         </div>
         <!--/end container-->
     </div>
@@ -22,14 +21,14 @@ include 'header.php';
             <div class="row">
                 <div class="col-md-8">
                     <div class="about-p1-cont">
-                        <h1>Business world best platform  </h1>
-                        <p> Our Business operates in more than two countries across five continents, with a mission 'To improve the quality of life of the communities we serve globally, through long-term stakeholder value creation based on Leadership with Trust' typesetting, remaining essentially unchanged.</p>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose. </p>
+                        <h1>Berkah Santoso  </h1>
+                        <p>Kami adalah Jasa Pondasi Perorangan yang bergerak dibidang jasa konstruksi dan sipil khususnya pekerjaan pondasi tiang pancang untuk rumah, gedung-gedung, tower, apartemen dan lain-lain dengan metoda Straus Pile dan Bore Pile. Dengan dukungan oleh Tenaga Ahli yang berpengalaman dan peralatan workshop modern yang lengkap.</p>
+                        <p>Kami mempunyai Visi dan Misi yang jelas. Serta komitmen untuk berkembang dan maju bersama dengan mengutamakan kualitas dan kepuasan pelanggan. Tujuan kami memberikan layanan yang terbaik kepada pelanggan dimanapun dan kapanpun. Serta menjadi mitra yang terbaik. </p>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="about-p1-img">
-                        <img src="img/ipad-tablet.png" class="img-fluid wow fadeInUp" data-wow-delay="0.1s" alt="...">
+                        <img src="img/pile_m.jpg" class="img-fluid wow fadeInUp" data-wow-delay="0.1s" alt="...">
                     </div>
                 </div>
             </div>
@@ -171,49 +170,40 @@ include 'header.php';
     <section id="comp-offer">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-3 col-sm-6  desc-comp-offer wow fadeInUp" data-wow-delay="0.2s">
-                    <h2>Latest News</h2>
+                <div class="col-md-12 col-sm-6  desc-comp-offer wow fadeInUp" data-wow-delay="0.2s">
+                    <h2>Portofolio</h2>
                     <div class="heading-border-light"></div>
-                    <button class="btn btn-general btn-green" role="button">See More</button>
+                    <!--button class="btn btn-general btn-green" role="button">See More</button-->
                 </div>
-                <div class="col-md-3 col-sm-6 desc-comp-offer wow fadeInUp" data-wow-delay="0.4s">
-                    <div class="desc-comp-offer-cont">
-                        <div class="thumbnail-blogs">
-                            <div class="caption">
-                                <i class="fa fa-chain"></i>
-                            </div>
-                            <img src="img/news/news-1.jpg" class="img-fluid" alt="...">
-                        </div>
-                        <h3>Pricing Strategies for Product</h3>
-                        <p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Business box. </p>
-                        <a href="#"><i class="fa fa-arrow-circle-o-right"></i> Learn More</a>
+                <div class="row">
+                  <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s"> 
+                    <div class="story-descb">
+                        <img src="img/tamansari.jpg" class="img-fluid" alt="...">
+                        <h6>The Hill Tamansari Recidence</h6>
+                        <p>Jasa kontruksi tiang pancang dengan Bored Pile untuk perumahan The Hill Tamansari Residence di daerah Tembalang, Mangunharjo, Kota Semarang.</p>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-6 desc-comp-offer wow fadeInUp" data-wow-delay="0.6s">
-                    <div class="desc-comp-offer-cont">
-                        <div class="thumbnail-blogs">
-                            <div class="caption">
-                                <i class="fa fa-chain"></i>
-                            </div>
-                            <img src="img/news/news-9.jpg" class="img-fluid" alt="...">
-                        </div>
-                        <h3>Design Exhibitions of 2017</h3>
-                        <p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Business box. </p>
-                        <a href="#"><i class="fa fa-arrow-circle-o-right"></i> Learn More</a>
+                  </div>
+                  <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s"> 
+                    <div class="story-descb">
+                        <img src="img/chalcedony.jpg" class="img-fluid" alt="...">
+                        <h6>Chalcedony Residence</h6>
+                        <p>Jasa kontruksi tiang pancang dengan Bored Pile untuk perumahan Summarecon Serpong Cluster Chalcedony Residence, Tangerang - Banten.</p>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-6 desc-comp-offer wow fadeInUp" data-wow-delay="0.8s">
-                    <div class="desc-comp-offer-cont">
-                        <div class="thumbnail-blogs">
-                            <div class="caption">
-                                <i class="fa fa-chain"></i>
-                            </div>
-                            <img src="img/news/news-12.jpeg" class="img-fluid" alt="...">
-                        </div>
-                        <h3>Exciting New Technologies</h3>
-                        <p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Business box. </p>
-                        <a href="#"><i class="fa fa-arrow-circle-o-right"></i> Learn More</a>
+                  </div>
+                  <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s"> 
+                    <div class="story-descb">
+                        <img src="img/jababeka.jpg" class="img-fluid" alt="...">
+                        <h6>Jababeka Residence</h6>
+                        <p>Jasa kontruksi tiang pancang menggunakan Strauss Pile untuk perumahan Jababeka Residence, Cikarang - Bekasi, Jawa Barat.</p>
                     </div>
+                  </div>  
+                  <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s"> 
+                    <div class="story-descb">
+                        <img src="img/alfamidi.jpg" class="img-fluid" alt="...">
+                        <h6>Kantor Alfamidi Surabaya</h6>
+                        <p>Jasa kontruksi tiang pancang menggunakan Bore Pile untuk gedung kantor Alfamidi cabang Surabaya.</p>
+                    </div>
+                  </div>       
                 </div>
             </div>
         </div>
